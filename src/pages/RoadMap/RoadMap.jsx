9906e2d53280926027417b64ab4d123fb94adcb6
Zsanett Tamás <zsanett.tamas87@gmail.com>
2023-04-16 22:18:19 +0200

    iconfy config

--- src/pages/RoadMap/RoadMap.jsx
+++ src/pages/RoadMap/RoadMap.jsx
@@ -1,31 +1,39 @@
+import React from "react";
+import { Icon } from "@iconify/react";
 import Avatar from "../../components/About/Avatar";
 import RoadMapData from "../../data/roadmap.json";
-import RoadMapImg from '../../assets/png/roadmap.png';
-
+import RoadMapImg from "../../assets/png/roadmap.png";
+const alt = "Freepik photo edited by Zsena: Roadmap image for a webdevelopment";
 
 const RoadMap = () => {
   return (
     <section className="container mx-auto pt-10">
-      <h1 className="text-white text-4xl py-10">Roadmap</h1>
+      <h1 className="text-4xl py-10">
+        Roadmap <Icon icon="vscode-icons:file-type-html" />
+      </h1>
 
-      <img className="mb-10 rounded-2xl max-w-[70%] mx-auto" src={RoadMapImg} alt="Freepik photo edited by Zsena, Roadmap image for a webdevelopment" />
+      <img
+        className="mb-10 rounded-2xl max-w-[70%] mx-auto"
+        src={RoadMapImg}
+        alt={alt}
+      />
       <section className="flex items-center justify-around flex-wrap">
-          {RoadMapData.map((roadMap) => {
-            return (
-              <Avatar
-                key={roadMap.id}
-                card={{
-                  name: roadMap.title,
-                  imageUrl: roadMap.imagePath,
-                  description: roadMap.excerpt,
-                  imageSize: 80,
-                  url: roadMap.url,
-                  tags: roadMap.tags
-                }}
-              />
-            );
-          })}
-        </section>
+        {RoadMapData.map((roadMap) => {
+          return (
+            <Avatar
+              key={roadMap.id}
+              card={{
+                name: roadMap.title,
+                imageUrl: roadMap.imagePath,
+                description: roadMap.excerpt,
+                imageSize: 80,
+                url: roadMap.url,
+                tags: roadMap.tags,
+              }}
+            />
+          );
+        })}
+      </section>
     </section>
   );
 };
