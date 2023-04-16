f2f0f33b952b6e66d8a0483077bdc96edf374063
Zsanett Tamás <zsanett.tamas87@gmail.com>
2023-04-16 13:13:35 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import { useState, useEffect } from "react";
-import Avatar from "./components/About/Avatar";
+// import { useState, useEffect } from "react";
+// import Avatar from "./components/About/Avatar";
 import NavRoute from "./components/Navbar/NavRoute";
 
-const user = {
-  name: "Keanu Reeves",
-  imageUrl:
-    "https://www.nme.com/wp-content/uploads/2022/07/Keanu-Reeves-new-696x442.jpg",
-  imageSize: 90,
-  description: "Lorem ipsum dolor sit amet ...",
-};
+
+// const user = {
+//   name: "Keanu Reeves",
+//   imageUrl:
+//     "https://www.nme.com/wp-content/uploads/2022/07/Keanu-Reeves-new-696x442.jpg",
+//   imageSize: 90,
+//   description: "Lorem ipsum dolor sit amet ...",
+// };
 
 const App = () => {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error));
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((json) => setData(json))
+  //     .catch((error) => console.error(error));
+  // }, []);
 
   return (
-    <main className="base-section flex flex-col justify-center pb-10">
-      <header className="mb-10">
-        <NavRoute />
-      </header>
+    <main className="base-section flex flex-col justify-center pb-10 h-screen">
+      <NavRoute />
       <section className="container mx-auto pt-10">
-        <section className="flex items-center justify-around flex-wrap">
+        {/* <section className="flex items-center justify-around flex-wrap">
           {data.map((userData) => {
             return (
               <Avatar
@@ -42,7 +41,7 @@ const App = () => {
               />
             );
           })}
-        </section>
+        </section> */}
       </section>
     </main>
   );
